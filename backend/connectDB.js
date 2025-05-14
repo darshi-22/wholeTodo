c36b0b75a6275e85401d87effc726db557304a71
Darshi Shah <149527523+darshi-22@users.noreply.github.com>
2025-05-14 19:52:22 +0530

    mongo commit

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -1,11 +1,16 @@
-const mongoose=require('mongoose');
-const connectDB=async()=>{
-    try{
-        await mongoose.connect('mongodb+srv://dpshah2209:darshi22@todo.ctvjxwn.mongodb.net/');
-        console.log('Mongodb connected');
+const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error(err);
     }
-    catch(err){
-        console.log(err);
-    }
-}
-module.exports=connectDB;
+};
+
+module.exports = connectDB;
